8e502f7589e8432f99cf50cbdb14f9007cc2b2ec
Vladimir Grinenko <i@tadatuta.com>
2017-07-29 02:07:51 +0300

    update

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const bb = require('bot-brother');
 const bot = bb({
-    key: process.env.KEY,
+    key: process.env.BOT_KEY,
     sessionManager: bb.sessionManager.memory(),
-    polling: { interval: 0, timeout: 1 }
+    // webHook: {},
+    polling: { interval: 0, timeout: 3 }
 });
+
+bot.use('before', bb.middlewares.typing());
+process.env.BOTANIO_KEY && bot.use('before', bb.middlewares.botanio(process.env.BOTANIO_KEY));
 
 const fingers = ['🖕', '🖖', '☝', '👆', '🖐️', '👌', '✌', '👍', '👈', '👉', '👇', '☝️'];
 
 const triggerQuestions = [
     'Смотрите внимательно!',
-    'Здесь главное — внимательность'
+    'Здесь главное — внимательность.',
+    'Это игра на внимательность!'
 ];
 
 const questions = [
@@ -23,7 +28,10 @@ const questions = [
 const triggerNoAnswers = [
     'Вы невнимательно смотрели!',
     'Будьте внимательнее!',
-    'Вам стоит быть повнимательнее!'
+    'Вам стоит быть повнимательнее!',
+    'Нет. Это игра на внимательность!',
+    'Увы... Чуть больше внимания и в следующий раз все получится.',
+    'Промах! Внимание и еще раз внимание!'
 ];
 
 const noAnswers = [
@@ -41,7 +49,8 @@ const noAnswers = [
 const triggerYesAnswers = [
     'Верно! Вы были внимательны!',
     'Правильно, вот что значит внимательность!',
-    'Вот, стоило лишь чуть внимательнее посмотреть и ура!'
+    'Вот, стоило лишь чуть внимательнее посмотреть и ура!',
+    'Верно! Это игра на внимательность!'
 ];
 
 const yesAnswers = [
@@ -63,12 +72,24 @@ const yesAnswers = [
     'Ай, молодца!'
 ];
 
+const finalParts = [
+    'А это',
+    'Тогда это',
+    'Как на счет этого',
+    'Это'
+];
+
+const fingerWords = [
+    'палец',
+    'пальчик'
+];
+
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function getRandom(collection) {
-    return collection[getRandomInt(0, collection.length - 1)];
+    return collection[getRandomInt(0, collection.length)];
 }
 
 const db = {};
@@ -76,34 +97,62 @@ const db = {};
 bot.command('start')
     .invoke(function(ctx) {
         const chatId = ctx.meta.chat.id;
+        const chat = db[chatId] || (db[chatId] = {});
+        const isBefore = Math.random() > 0.4;
         let question = '';
 
-        if (typeof db[chatId] === 'undefined') {
-            const isFinger = Math.random() > 0.8;
-            db[chatId] = isFinger;
+        if (typeof chat.state === 'undefined' || chat.isFromStat) {
+            const isFinger = Math.random() > 0.4;
+            chat.state = isFinger;
+
+            if (chat.isFromStat) {
+                chat.isFromStat = false;
+            } else {
+                chat.users = {};
+            }
 
             question = (isFinger ?
                 getRandom(triggerQuestions) :
-                getRandom(questions)) + ' ';
+                getRandom(questions)) + '\n';
         }
 
-        return ctx.sendMessage(question + [
-            getRandom(fingers),
-            getRandom(fingers),
-            getRandom(fingers),
-            'а это?'
-        ].join(', '));
+        return ctx.sendMessage(
+            (isBefore ? question : '') +
+            getRandom(fingers) + ' — ' + getRandom(fingerWords) + ',\n' +
+            getRandom(fingers) + ' — ' + getRandom(fingerWords) + ',\n' +
+            getRandom(fingers) + ' — ' + getRandom(fingerWords) + '.\n' +
+            getRandom(finalParts) + ' ' + getRandom(fingers) + '?' +
+            (isBefore ? '' : ('\n' + question))
+        );
     })
     .keyboard([
         [{ 'Палец': { value: true } }],
         [{ 'Не палец': { value: false } }]
     ])
     .answer(function(ctx) {
-        const chatId = ctx.meta.chat.id;
-        const isRight = db[chatId] === ctx.answer;
+        ctx.hideKeyboard();
 
-        const isFinger = Math.random() > 0.8;
-        db[chatId] = isFinger;
+        const chatId = ctx.meta.chat.id;
+        const username = ctx.meta.user.username;
+
+        const isRight = db[chatId].state === ctx.answer;
+
+        const isFinger = Math.random() > 0.55;
+        db[chatId].state = isFinger;
+        db[chatId].users[username] || (db[chatId].users[username] = {
+            win: 0,
+            fail: 0,
+            sequence: 0
+        });
+
+        const user = db[chatId].users[username];
+        if (isRight) {
+            user.win += 1;
+            user.sequence += 1;
+        } else {
+            user.fail += 1;
+            user.sequence = 0;
+        }
 
         const answer = isFinger ?
             getRandom(isRight ? triggerYesAnswers : triggerNoAnswers) :
@@ -113,3 +162,23 @@ bot.command('start')
             .sendMessage(answer)
             .then(() => ctx.go('start'));
     });
+
+bot.command('stat')
+    .invoke(function(ctx) {
+        const chatId = ctx.meta.chat.id;
+        const users = db[chatId].users;
+
+        if (!db[chatId].users) return;
+
+        db[chatId].isFromStat = true;
+
+        ctx.sendMessage(Object.keys(db[chatId].users).map(username => {
+            const user = users[username];
+            return '@' + username +
+                '\nПобед: ' + user.win + ' (подряд: ' + user.sequence +
+                ')\nПоражений: ' + user.fail;
+        }).join('\n'));
+    })
+    .keyboard([
+        [{ 'MOAR!': { go: 'start' } }]
+    ]);
